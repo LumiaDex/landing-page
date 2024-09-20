d47172524236bf6ac01704248d7d193db048e4f5
Nguyen Hoang Man <hoangman772@gmail.com>
2024-09-20 11:26:12 +0700

    Update mobile and desktop

--- src/pages/LandingPage/Footer.tsx
+++ src/pages/LandingPage/Footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <div className="w-full relative">
       <img className="w-full" src={FooterSession1Img} />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer group">
-        <div className="md:text-[96px] leading-[48px] w-max md:w-full text-[48px] font-normal text-center group-hover:text-primary transition-all duration-300">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer group flex items-center justify-center flex-col">
+        <div className="lg:text-[96px] leading-[48px] md:text-[64px] w-max lg:w-full text-[48px] font-normal text-center group-hover:text-primary transition-all duration-300">
           Enter Cassus
         </div>
         <div className="md:text-2xl text-base font-normal mt-2 md:mt-6 text-center md:leading-[40px] leading-[20px] group-hover:text-primary transition-all duration-300">
