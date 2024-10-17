fb1e3bb60e4bca15d58f66480c7f000f01a2d051
vodinhvien <vodinhvien.work@gmail.com>
2024-10-17 18:05:50 +0700

    fix: build

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,10 +6,14 @@ export const Card = ({
 }: {
   children: ReactNode;
   className?: string;
-}) => (
-  <div className="bg-[#ADCBF3]/[0.05] relative z-[1] rounded-[24px] p-[12px] border border-[#ADCBF3]/[0.06] h-full w-full">
-    <div className="bg-radial-gradient relative z-[1] rounded-[14px] border border-[#ADCBF3]/[0.15] p-0 flex flex-col h-full w-full items-center gap-[20px] xl:p-[20px]">
-      {children}
+}) => {
+  console.log(className);
+
+  return (
+    <div className="bg-[#ADCBF3]/[0.05] relative z-[1] rounded-[24px] p-[12px] border border-[#ADCBF3]/[0.06] h-full w-full">
+      <div className="bg-radial-gradient relative z-[1] rounded-[14px] border border-[#ADCBF3]/[0.15] p-0 flex flex-col h-full w-full items-center gap-[20px] xl:p-[20px]">
+        {children}
+      </div>
     </div>
-  </div>
-);
+  );
+};
