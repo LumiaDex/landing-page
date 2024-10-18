43e4238e36707883223515d7493a2ed72d625a3b
vodinhvien <vodinhvien.work@gmail.com>
2024-10-18 17:44:31 +0700

    feat: link

--- src/pages/LandingPage/Footer.tsx
+++ src/pages/LandingPage/Footer.tsx
@@ -6,25 +6,30 @@ const socials = [
   {
     name: "Telegram",
     icon: TelegramIcon,
+    link: "",
   },
   {
     name: "Discord",
     icon: DiscordIcon,
+    link: "",
   },
   {
     name: "X",
     icon: XIcon,
+    link: "https://x.com/ilumfi",
   },
 ];
 export default function Footer() {
   return (
-    <div className="w-full relative flex flex-col md:flex-row items-center justify-center gap-3 mb-[30px] md:justify-between max-w-[1170px] mx-auto px-4 xl:px-0">
+    <div className="w-full z-50 relative flex flex-col md:flex-row items-center justify-center gap-3 mb-[30px] md:justify-between max-w-[1170px] mx-auto px-4 xl:px-0">
       <div className="text-xs text-[#97A1C0]">
         All rigths reserved. © 2024 ILUM.FI
       </div>
       <div className="flex gap-4">
-        {socials.map(({ name, icon: Icon }) => (
-          <Icon key={name} className="cursor-pointer" />
+        {socials.map(({ name, icon: Icon, link }) => (
+          <a href={link} key={name} target="_blank">
+            <Icon className="cursor-pointer" />
+          </a>
         ))}
       </div>
     </div>
